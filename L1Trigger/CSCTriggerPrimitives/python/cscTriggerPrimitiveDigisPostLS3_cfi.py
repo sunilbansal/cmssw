31db8a81bdd0eb699376a29e47f2fcbad8aae370
tahuang1991 <taohuang@physics.tamu.edu>
2014-06-20 12:58:31 -0500

    change parameter set to get suitable matching windows

--- L1Trigger/CSCTriggerPrimitives/python/cscTriggerPrimitiveDigisPostLS3_cfi.py
+++ L1Trigger/CSCTriggerPrimitives/python/cscTriggerPrimitiveDigisPostLS3_cfi.py
@@ -380,8 +380,8 @@ cscTriggerPrimitiveDigisPostLS3 = cms.EDProducer("CSCTriggerPrimitivesProducer",
         dropLowQualityALCTsNoGEMs_ME1b = cms.bool(False),
         buildLCTfromALCTandGEM_ME1a = cms.bool(True),
         buildLCTfromALCTandGEM_ME1b = cms.bool(True),
-        buildLCTfromCLCTandGEM_ME1a = cms.bool(True),
-        buildLCTfromCLCTandGEM_ME1b = cms.bool(True),
+        buildLCTfromCLCTandGEM_ME1a = cms.bool(False),
+        buildLCTfromCLCTandGEM_ME1b = cms.bool(False),
         doLCTGhostBustingWithGEMs = cms.bool(False),
         correctLCTtimingWithGEM = cms.bool(False),
         promoteALCTGEMpattern = cms.bool(True),
@@ -433,23 +433,23 @@ cscTriggerPrimitiveDigisPostLS3 = cms.EDProducer("CSCTriggerPrimitivesProducer",
         
         ## copad construction
         maxDeltaBXInCoPad = cms.int32(1),
-        maxDeltaPadInCoPad = cms.int32(1),
+        maxDeltaPadInCoPad = cms.int32(2),
 
         ## matching to pads in case LowQ CLCT
         maxDeltaBXPad = cms.int32(1),
-        maxDeltaPadPadOdd = cms.int32(3),
-        maxDeltaPadPadEven = cms.int32(2),
+        maxDeltaPadPadOdd = cms.int32(4),
+        maxDeltaPadPadEven = cms.int32(3),
         maxDeltaWg = cms.int32(1),
 
         ## matching to pads in case absent CLCT
-        maxDeltaBXCoPad = cms.int32(0),
+        maxDeltaBXCoPad = cms.int32(1),
         maxDeltaPadCoPad = cms.int32(2),
 
         ## efficiency recovery switches
         dropLowQualityALCTsNoGEMs = cms.bool(False),
         dropLowQualityCLCTsNoGEMs = cms.bool(True),
         buildLCTfromALCTandGEM = cms.bool(True),
-        buildLCTfromCLCTandGEM = cms.bool(True),
+        buildLCTfromCLCTandGEM = cms.bool(False),
         doLCTGhostBustingWithGEMs = cms.bool(False),
         correctLCTtimingWithGEM = cms.bool(False),
         promoteALCTGEMpattern = cms.bool(True),
@@ -497,14 +497,14 @@ cscTriggerPrimitiveDigisPostLS3 = cms.EDProducer("CSCTriggerPrimitivesProducer",
 
         ## matching to digis in case LowQ CLCT
         maxDeltaBXRPC = cms.int32(0),
-        maxDeltaStripRPCOdd = cms.int32(5),
+        maxDeltaStripRPCOdd = cms.int32(6),
         maxDeltaStripRPCEven = cms.int32(4),
         maxDeltaWg = cms.int32(1),
 
         ## efficiency recovery switches
         dropLowQualityCLCTsNoRPCs = cms.bool(True),
         buildLCTfromALCTandRPC = cms.bool(True),
-        buildLCTfromCLCTandRPC = cms.bool(True),
+        buildLCTfromCLCTandRPC = cms.bool(False),
         buildLCTfromLowQstubandRPC = cms.bool(True),
         promoteCLCTRPCquality = cms.bool(True),
         promoteALCTRPCpattern = cms.bool(True),
