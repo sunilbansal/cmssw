ace79a73aa520774675c26b76d0e416cc04028a7
Cesare <cesare.calabria@ba.infn.it>
2015-07-06 14:38:02 +0200

    remove uneeded files

--- L1IntegratedMuonTrigger/python/MBLTProducer_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-MBLTProducer = cms.EDProducer(
-    'MBLTProducer',
-    TriggerPrimitiveSrc = cms.InputTag('L1ITMuTriggerPrimitives'),
-    MaxDeltaPhi = cms.double( 0.05 )
-    )
--- L1IntegratedMuonTrigger/src/TrackSeeder.cc
+++
@@ -1,9 +0,0 @@
-#include "L1Trigger/L1IntegratedMuonTrigger/interface/TrackSeeder.h"
-
-using namespace L1ITMu;
-
-TrackSeeder::TrackSeeder() {
-}
-
-TrackSeeder::~TrackSeeder() {
-}
