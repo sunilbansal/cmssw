30913bb4c359d3e56d2eaf5dcfc093b629589274
Sven Dildick <sven.dildick@cern.ch>
2016-04-19 13:49:14 -0500

    Include RPC trigger digis

--- L1Trigger/L1TMuonEndCap/python/simEmtfDigis_cfi.py
+++ L1Trigger/L1TMuonEndCap/python/simEmtfDigis_cfi.py
@@ -3,7 +3,8 @@ import FWCore.ParameterSet.Config as cms
 ###EMTF emulator configuration
 simEmtfDigis = cms.EDProducer("L1TMuonEndCapTrackProducer",
                               CSCInput = cms.InputTag('simCscTriggerPrimitiveDigis','MPCSORTED'),
-                              GEMInput = cms.InputTag('simMuonGEMPadDigis')
+                              GEMInput = cms.InputTag('simMuonGEMPadDigis'),
+                              RPCInput = cms.InputTag('simMuonRPCDigis')
                               )
 
 
