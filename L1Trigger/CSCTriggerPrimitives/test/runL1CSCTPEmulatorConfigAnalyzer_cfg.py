d3281971ca9037da2b9d052a87d2cd40bd48eb4d
Sven Dildick <sven.dildick@cern.ch>
2016-05-03 00:53:36 +0200

    u

--- L1Trigger/CSCTriggerPrimitives/test/runL1CSCTPEmulatorConfigAnalyzer_cfg.py
+++ L1Trigger/CSCTriggerPrimitives/test/runL1CSCTPEmulatorConfigAnalyzer_cfg.py
@@ -1,8 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("TEST")
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process("TEST",eras.Run2_25ns)
 
 process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('L1Trigger.CSCTriggerPrimitives.cscTriggerPrimitiveDigis_cfi')
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
@@ -12,13 +15,10 @@ process.maxEvents = cms.untracked.PSet(
 )
 process.source = cms.Source("EmptySource")
 
-
-from L1Trigger.CSCTriggerPrimitives.cscTriggerPrimitiveDigis_cfi import cscTriggerPrimitiveDigis
-
 process.L1CSCTPEmulatorConfigAnalyzer = cms.EDAnalyzer("L1CSCTPEmulatorConfigAnalyzer",
-                                               alctParam07 = cscTriggerPrimitiveDigis.alctParam07,
-                                               clctParam07 = cscTriggerPrimitiveDigis.clctParam07,
-                                               tmbParam = cscTriggerPrimitiveDigis.tmbParam,
+    alctParam07 = process.cscTriggerPrimitiveDigis.alctParam07,
+    clctParam07 = process.cscTriggerPrimitiveDigis.clctParam07,
+    tmbParam = process.cscTriggerPrimitiveDigis.tmbParam,
 )
 
 process.p = cms.Path(process.L1CSCTPEmulatorConfigAnalyzer)
